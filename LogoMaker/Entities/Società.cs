ee48b9663014ed9e096b50838ab4ec8ed1242bdd
Francesco Galavotti <f.galavotti@cloudsurfers.it>
2026-04-09 14:37:16 +0200

    inizializzazione prime entities

---
+++ LogoMaker/Entities/Società.cs
@@ -0,0 +1,10 @@
+﻿
+namespace LogoMaker.Entities;
+
+public class Società
+{
+    public string RagioneSociale{ get; set; }
+    public string PartitaIVA{ get; set; }
+    public string Logo{ get; set; }
+    public Utente? User{ get; set; }
+}
---
+++ LogoMaker/Entities/Utente.cs
@@ -0,0 +1,11 @@
+﻿
+namespace LogoMaker.Entities;
+
+public class Utente
+{
+    public string Username{ get; set; }
+    public string Password{ get; set; }
+    public string Gender{ get; set; }
+    public string Role{ get; set; }
+    public List<Società>? SocietàUtente{ get; set; }
+}
